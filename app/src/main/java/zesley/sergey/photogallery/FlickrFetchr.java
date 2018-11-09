474546b6fa8abd7fea2ac9b10ffbf116a975c560
Zes <sergey_the_great@mail.ru>
2018-11-10 02:41:23 +0500

    Сделал до страницы 435. Change KEY.

--- app/src/main/java/zesley/sergey/photogallery/FlickrFetchr.java
+++ app/src/main/java/zesley/sergey/photogallery/FlickrFetchr.java
@@ -17,7 +17,7 @@ import java.util.List;
 
 public class FlickrFetchr {
     private static final String TAG = "FlickrFetchr";
-    private static final String API_KEY = "ваш_ключ_Api";
+    private static final String API_KEY = "a3fc39d98e1fc91cc1ac9f8623f734bd";
     private static final String FETCH_RECENTS_METHOD = "flickr.photos.getRecent";
     private static final String SEARCH_METHOD = "flickr.photos.search";
     private static final Uri ENDPOINT = Uri
