511a7db54c71e2ed7830e5125ceaa11d25f46329
Zes <sergey_the_great@mail.ru>
2018-11-10 19:57:17 +0500

    Сделал до страницы 520.

--- app/src/main/java/zesley/sergey/photogallery/PollService.java
+++ app/src/main/java/zesley/sergey/photogallery/PollService.java
@@ -2,11 +2,15 @@ package zesley.sergey.photogallery;
 
 import android.app.AlarmManager;
 import android.app.IntentService;
+import android.app.Notification;
 import android.app.PendingIntent;
 import android.content.Intent;
 import android.content.Context;
+import android.content.res.Resources;
 import android.net.ConnectivityManager;
 import android.os.SystemClock;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.NotificationManagerCompat;
 import android.util.Log;
 
 import java.util.List;
@@ -24,6 +28,7 @@ public class PollService extends IntentService {
     private static final String TAG = "PollService";
     private static final int POLL_INTERVAL = 1000 * 60; // 60 секунд
     private static final long POLL_INTERVAL_MS = TimeUnit.MINUTES.toMillis(1);
+
     public static Intent newIntent(Context context) {
         return new Intent(context, PollService.class);
     }
@@ -63,6 +68,13 @@ public class PollService extends IntentService {
             Log.i(TAG, "Got an old result: " + resultId);
         } else {
             Log.i(TAG, "Got a new result: " + resultId);
+            Resources resources = getResources();
+            Intent i = PhotoGalleryActivity.newIntent(this);
+            PendingIntent pi = PendingIntent.getActivity(this, 0, i, 0);
+            Notification notification = new NotificationCompat.Builder(this).setTicker(resources.getString(R.string.new_pictures_title)).setSmallIcon(android.R.drawable.ic_menu_report_image).setContentTitle(resources.getString(R.string.new_pictures_title)).setContentText(resources.getString(R.string.new_pictures_text)).setContentIntent(pi).setAutoCancel(true).build();
+            NotificationManagerCompat notificationManager = NotificationManagerCompat.from(this);
+            notificationManager.notify(0, notification);
+
         }
         QueryPreferences.setLastResultId(this, resultId);
     }
@@ -88,5 +100,6 @@ public class PollService extends IntentService {
         } else {
             alarmManager.cancel(pi);
             pi.cancel();
-        } }
+        }
+    }
 }
